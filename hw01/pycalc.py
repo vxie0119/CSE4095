f48229f2b193d2b7f56dda4b0ad6f9a9f496552e
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-01-23 00:19:47 -0500

    Wrote code for common operators

--- hw01/pycalc.py
+++ hw01/pycalc.py
@@ -1,1 +1,14 @@
-print("testing files going into repo")
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return x - y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    if y != 0:
+        return x/y
+    else:
+        return "Cannot divide by zero!"
