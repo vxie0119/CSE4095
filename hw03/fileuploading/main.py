31fc11e5f70ccae093dc8cfa90b81e20cd56d263
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-08 20:44:20 -0500

    Some changes

--- hw03/fileuploading/main.py
+++ hw03/fileuploading/main.py
@@ -1,6 +1,6 @@
 from distutils.log import debug
 from fileinput import filename
-from flask import *
+from flask import Flask
 from datetime import datetime
 import os 
 
@@ -37,4 +37,4 @@ def get_files():
 
 
 if __name__ == '__main__':
-    app.run(debug = True, host='0.0.0.0', port=8080)
+    app.run(debug = True, host='0.0.0.0')
