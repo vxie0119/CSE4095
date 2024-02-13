d44e9a057ff93a716b0f9cd9e89cb34acc9261f4
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-13 16:47:52 -0500

    Code for listing contents

--- hw04/application.py
+++ hw04/application.py
@@ -1,3 +1,8 @@
 import boto3
 import os
 
+def list_contents(bucket, s3_client):
+    contents = []
+    for item in s3_client.list_objects_v2(Bucket=bucket)['Contents']:
+        contents.append(item['Key'])
+    return contents
