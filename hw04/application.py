09a3567a609fe73751954fd40b9ad5fee85d91f7
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-13 16:46:14 -0500

    Code for upload function

--- hw04/application.py
+++ hw04/application.py
@@ -1,3 +1,9 @@
 import boto3
 import os
 
+def upload(local, bucket, s3_client):
+    for subdir, dirs, files in os.walk(local):
+        for file in files:
+            full_path = os.path.join(subdir, file)
+            with open(full_path, 'rb') as data:
+                s3_client.upload_fileobj(data, bucket, full_path[len(local):])
