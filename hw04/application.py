b8f64cd91db48f7254e4ecb9fb55e365c3b1889a
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-13 21:55:14 -0500

    Code that finall works

--- hw04/application.py
+++ hw04/application.py
@@ -14,15 +14,15 @@ def main_menu():
         choice = input("Enter your choice:")
 
         if choice == '1':
-            list_buckets()
+            list_buckets(s3)
         elif choice == '2':
             local = input('Enter the path to the local folder: ')
             bucket = input('Enter the bucket name: ')
-            upload(local, bucket)
+            upload(s3, local, bucket)
         elif choice == '3':
             bucket = input('Enter the bucket name: ')
             try:
-                contents = list_contents(bucket)
+                contents = list_contents(s3, bucket)
                 for item in contents:
                     print(item)
             except ClientError as e:
@@ -30,51 +30,59 @@ def main_menu():
         elif choice == '4':
             bucket = input('Enter the bucket name: ')
             file = input('Enter the file name: ')
-            get_file(bucket, file)
+            get_file(s3, bucket, file)
         elif choice == '5':
             break
         else:
             print('Invalid choice.')
          
-def upload(local, bucket):
-    s3_client = boto3.client('s3')
-    for subdir, dirs, files in os.walk(local):
-        for file in files:
-            full_path = os.path.join(subdir, file)
-            key = os.path.relpath(full_path, local)
-            try:
-                with open(full_path, 'rb') as data:
-                    s3_client.upload_fileobj(data, bucket, key)
-                print(f"Uploaded {full_path} to {bucket}/{key}")
-            except FileNotFoundError:
-                print(f"File not found: {full_path}")
-            except ClientError as e:
-                print(f"An error occurred uploading {full_path}: {e}")
-            except Exception as e:
-                print(f"Unexpected error: {e}")
-     
-def list_contents(bucket):
-    s3_client = boto3.client('s3')
-    response = s3_client.list_objects_v2(Bucket=bucket)
+def upload(s3, local_path, bucket):
+    local_path = os.path.expanduser(local_path)  # Expand the ~ symbol
+
+    # Check if the path is a directory
+    if os.path.isdir(local_path):
+        # If it's a directory, upload each file
+        for subdir, dirs, files in os.walk(local_path):
+            for file in files:
+                full_path = os.path.join(subdir, file)
+                key = os.path.relpath(full_path, local_path)
+                try:
+                    s3.upload_file(full_path, bucket, key)
+                    print(f"Uploaded {file} to {bucket}/{key}")
+                except ClientError as e:
+                    print(f"An error occurred: {e}")
+    elif os.path.isfile(local_path):
+        # If it's a file, just upload the file
+        try:
+            file_name = os.path.basename(local_path)
+            s3.upload_file(local_path, bucket, file_name)
+            print(f"Uploaded {file_name} to {bucket}/{file_name}")
+        except ClientError as e:
+            print(f"An error occurred: {e}")
+    else:
+        print(f"The provided path does not exist: {local_path}")
+
+    
+    
+def list_contents(s3, bucket):
+    response = s3.list_objects_v2(Bucket=bucket)
     if 'Contents' in response:
         return [item['Key'] for item in response['Contents']]
     else:
         return []
  
-def get_file(bucket, file):
+def get_file(s3, bucket, file):
     try:
-        s3_client = boto3.client('s3')
         download_path = os.path.join(os.getcwd(), file)
         with open(file, 'wb') as data:
-            s3_client.download_fileobj(bucket, file, data)
+            s3.download_fileobj(bucket, file, data)
         print(f"File '{file}' downloaded successfully.")
     except ClientError as e:
         print(f"An error occurred: {e}")
 
 
-def list_buckets():
+def list_buckets(s3):
     try:
-        s3 = boto3.client('s3')
         response = s3.list_buckets()
         print("Bucket List: ")
         for bucket in response['Buckets']:
