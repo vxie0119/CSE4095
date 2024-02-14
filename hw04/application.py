07f0dd4a00625f5a3862e83a50b3434be6dd96b9
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-13 21:19:53 -0500

    Fixed small errors

--- hw04/application.py
+++ hw04/application.py
@@ -18,11 +18,11 @@ def main_menu():
         elif choice == '2':
             local = input('Enter the path to the local folder: ')
             bucket = input('Enter the bucket name: ')
-            upload(local, bucket, s3)
+            upload(local, bucket)
         elif choice == '3':
             bucket = input('Enter the bucket name: ')
             try:
-                contents = list_contents(bucket, s3)
+                contents = list_contents(bucket)
                 for item in contents:
                     print(item)
             except ClientError as e:
@@ -30,7 +30,7 @@ def main_menu():
         elif choice == '4':
             bucket = input('Enter the bucket name: ')
             file = input('Enter the file name: ')
-            get_file(bucket, file, s3)
+            get_file(bucket, file)
         elif choice == '5':
             break
         else:
@@ -54,10 +54,15 @@ def list_contents(bucket):
         return []
  
 def get_file(bucket, file):
-    s3_client = boto3.client('s3')
-    download_path = os.path.join(os.getcwd(), file_name)
-    with open(file, 'wb') as data:
-        s3_client.download_fileobj(bucket, file, data)
+    try:
+        s3_client = boto3.client('s3')
+        download_path = os.path.join(os.getcwd(), file)
+        with open(file, 'wb') as data:
+            s3_client.download_fileobj(bucket, file, data)
+        print(f"File '{file}' downloaded successfully.")
+    except ClientError as e:
+        print(f"An error occurred: {e}")
+
 
 def list_buckets():
     try:
