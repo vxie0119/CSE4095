295de3e15d686bd74cbe8a6b9483a29b0a049678
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-27 20:58:41 -0500

    first version of the code

--- hw06/lambda_delete.py
+++ hw06/lambda_delete.py
@@ -1,8 +1,16 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
     # TODO implement
+    s3 = boto3.client('s3')
+    bucket = event['pathParameters']['bucket-name']
+    object = event['pathParameters']['object-name']
+
+    # Delete the object from the bucket
+    s3.delete_object(Bucket=bucket, Key=object)
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps({'message': f'{object} deleted from {bucket}'})
     }
