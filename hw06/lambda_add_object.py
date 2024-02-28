7162452a6ebdc22f70eb50f7b5b3b3f27286b748
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-27 20:47:18 -0500

    first version of add object code

--- hw06/lambda_add_object.py
+++ hw06/lambda_add_object.py
@@ -1,8 +1,20 @@
 import json
+import boto3
+from urllib.parse import unquote_plus
 
 def lambda_handler(event, context):
     # TODO implement
+    s3 = boto3.client('s3')
+    bucket = event['pathParameters']['bucket-name']
+    body = json.loads(event['body'])
+
+    object_name = unquote_plus(body['name'])
+    object_content = body['content']
+
+    # Add the object to the bucket
+    s3.put_object(Bucket=bucket, Key=object_name, Body=object_content)
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps({'message': f'{object_name} added to {bucket}'})
     }
