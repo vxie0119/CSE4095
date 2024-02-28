6dda3660d3e8d74d58a39c831094fc6920aff6ff
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-27 20:30:50 -0500

    First version of lambda code

--- hw06/lambda_list_buckets.py
+++ hw06/lambda_list_buckets.py
@@ -3,7 +3,13 @@ import boto3
 
 def lambda_handler(event, context):
     # TODO implement
+    s3 = boto3.client('s3')
+    buckets = s3.list_buckets()
+
+    # Names
+    names = [bucket['Name'] for bucket in buckets['Buckets']]
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(names)
     }
