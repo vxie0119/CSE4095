4ade8bb86c9af257d544f0e160edb424a06d7a1c
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-27 20:26:17 -0500

    Starting code for lambda function

---
+++ hw06/lambda_list_buckets.py
@@ -0,0 +1,9 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
