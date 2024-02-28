233561e7fca6790bd9b8ad0aaf250d5e6cd3935c
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-27 21:34:00 -0500

    changed code so it works with mapping template

--- hw06/lambda_add_object.py
+++ hw06/lambda_add_object.py
@@ -8,28 +8,28 @@ logger.setLevel(logging.INFO)
 
 s3_client = boto3.client('s3')
 
-response  = {
-    'statusCode': 200,
-    'headers': {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': 'true'
-    },
-    'body': ''
-}
-
 def lambda_handler(event, context):
-
-    file_name = event['headers']['file-name']
-    file_content = base64.b64decode(event['body'])
+    bucket_name = event['bucket']
+    file_name = event['file_name']
     
-    BUCKET_NAME = os.environ['BUCKET_NAME']
+    # The file content is expected to be base64-encoded in the body
+    file_content = base64.b64decode(event['body'])
 
     try:
         s3_response = s3_client.put_object(Bucket=BUCKET_NAME, Key=file_name, Body=file_content)   
         logger.info('S3 Response: {}'.format(s3_response))
-        response['body'] = 'Your file has been uploaded'
+        response_body = 'Your file has been uploaded'
 
-        return response
 
     except Exception as e:
-        raise IOError(e)
+        logger.error("Error uploading file: {}".format(e))
+        response_body = 'Error in fiel upload'
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': 'true'
+        },
+        'body': json.dumps({'message': response_body})
+    }
