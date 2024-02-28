fd843c3580310d894b1a10cb619acc39e68e4cff
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-27 21:45:08 -0500

    Handling post body template

--- hw06/lambda_add_object.py
+++ hw06/lambda_add_object.py
@@ -1,7 +1,7 @@
 import logging
 import base64
 import boto3
-import os
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -9,14 +9,14 @@ logger.setLevel(logging.INFO)
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
-    bucket_name = event['bucket']
-    file_name = event['file_name']
+    bucket_name = event['body-json']['bucket']
+    file_name = event['body-json']['file_name']
     
     # The file content is expected to be base64-encoded in the body
     file_content = base64.b64decode(event['body'])
 
     try:
-        s3_response = s3_client.put_object(Bucket=BUCKET_NAME, Key=file_name, Body=file_content)   
+        s3_response = s3_client.put_object(Bucket=bucket_name, Key=file_name, Body=file_content)   
         logger.info('S3 Response: {}'.format(s3_response))
         response_body = 'Your file has been uploaded'
 
