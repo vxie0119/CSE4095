e76e26dec11cab9c2f4bd848e0384004b16be9c2
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-27 20:35:14 -0500

    Code before testing

--- hw06/lambda_list_objects.py
+++ hw06/lambda_list_objects.py
@@ -1,8 +1,16 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
     # TODO implement
+    s3 = boto3.client('s3')
+    bucket = event['pathParameters']['bucket-name']
+
+    # List objects within the bucket
+    objects = s3.list_objects_v2(Bucket=bucket)
+    object_keys =  [obj['Key'] for obj in objects.get('Contents', [])]
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(object_keys)
     }
