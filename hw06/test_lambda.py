57f1cacffd5c610792d1a8f37f4d9bb5c0ae4315
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-28 22:45:50 -0500

    current test file

--- hw06/test_lambda.py
+++ hw06/test_lambda.py
@@ -15,13 +15,26 @@ LIST_OBJECTS_URL = f"{API_URL}/{BUCKET}"
 POST_OBJECT_URL = f"{API_URL}/{BUCKET}"
 DELETE_OBJECT_URL = f"{API_URL}/{BUCKET}/{FILE_NAME}"
 
-# This can be used if your Lambda function is expected to return these responses
-SUCCESS_RESPONSE = {"statusCode": 200, "body": json.dumps({"message": "success"})}
-FAILURE_RESPONSE = {"statusCode": 400, "body": json.dumps({"message": "failure"})}
 
-# Test GET request to list objects
+
+def test_list_buckets():
+    response = get(LIST_BUCKET_URL)
+    assert response['statusCode'] == 200
+
 def test_list_objects():
-    expected_data = create_post_data_for_list_objects(BUCKET, FILE_NAME)
     response = get(LIST_OBJECTS_URL)
-    # Assert statements should compare the response to expected values
-    assert response == SUCCESS_RESPONSE
+    assert response['statusCode'] == 200
+
+def test_add_object():
+    file_content = read_file_into_base64_string(FILE_PATH)
+    post_data = create_post_data_for_post_object(BUCKET, FILE_NAME, file_content)
+    response = post(POST_OBJECT_URL, post_data)
+    assert response['statusCode'] == 200
+
+def test_delete_object():
+    data = create_data_for_del_object(BUCKET, FILE_NAME)
+    response = delete(DELETE_OBJECT_URL, data)
+    assert response['statusCode'] == 200
+
+if __name__ == "__main__":
+    pytest.main()
