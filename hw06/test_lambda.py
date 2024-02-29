9251435db4fbccb8fb6370d6fd7325b0a81c08b6
Vincent Xie <vxie0119@Vincents-Air.lan>
2024-02-28 22:52:47 -0500

    test file

--- hw06/test_lambda.py
+++ hw06/test_lambda.py
@@ -6,14 +6,14 @@ from http_utils import *
 # Define constants for the test
 API_URL = "https://qppgh6p2uk.execute-api.us-east-1.amazonaws.com/prod"
 BUCKET = "hw06.s3"
-FILE_NAME = "giants.png"
+OBJECT_NAME = "giants.png"
 FILE_PATH = "./hw06/giants.png"  
 
 # Replace these with the actual endpoints or the mock endpoints if you are mocking
 LIST_BUCKET_URL = f"{API_URL}/list"
 LIST_OBJECTS_URL = f"{API_URL}/{BUCKET}"
 POST_OBJECT_URL = f"{API_URL}/{BUCKET}"
-DELETE_OBJECT_URL = f"{API_URL}/{BUCKET}/{FILE_NAME}"
+DELETE_OBJECT_URL = f"{API_URL}/{BUCKET}/{OBJECT_NAME}"
 
 
 
@@ -27,12 +27,12 @@ def test_list_objects():
 
 def test_add_object():
     file_content = read_file_into_base64_string(FILE_PATH)
-    post_data = create_post_data_for_post_object(BUCKET, FILE_NAME, file_content)
+    post_data = create_post_data_for_post_object(BUCKET, OBJECT_NAME, file_content)
     response = post(POST_OBJECT_URL, post_data)
     assert response['statusCode'] == 200
 
 def test_delete_object():
-    data = create_data_for_del_object(BUCKET, FILE_NAME)
+    data = create_data_for_del_object(BUCKET, OBJECT_NAME)
     response = delete(DELETE_OBJECT_URL, data)
     assert response['statusCode'] == 200
 
