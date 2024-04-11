83aa91b6d12f8e5c7e518bb07ed6e595e58f92f5
Vincent Xie <vxie0119@MacBook-Pro.lan>
2024-04-11 00:04:09 -0400

    current version of code

--- hw09/thumbnail_generator.py
+++ hw09/thumbnail_generator.py
@@ -1,8 +1,32 @@
-import json
+import boto3
+import os
+from PIL import Image
+import io
 
-def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+s3_client = boto3.client('s3')
+
+def thumbnail_generator(event, context):
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        image = get_image(bucket, key)
+        thumbnail = create_thumbnail(image)
+        s3_save(bucket, key, thumbnail)
+
+def get_image(bucket, key):
+    response = s3_client.get_object(Bucket=bucket, Key=key)
+    content = response['Body'].read()
+    image = Image.open(io.BytesIO(content))
+    return image
+
+def create_thumbnail(image):
+    thumbnail_size = (128, 128)
+    image.thumbnail(thumbnail_size)
+    buffer = io.BytesIO()
+    image.save(buffer, 'JPEG')
+    buffer.seek(0)
+    return buffer
+
+def s3_save(bucket, key, thumbnail):
+    thumbnail_key = 'thumbnails/' + os.path.basename(key)
+    s3_client.put_object(Bucket=bucket, Key=thumbnail_key, Body=thumbnail)
