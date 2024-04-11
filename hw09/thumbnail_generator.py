08bdcef2e038616cae0c096ee37fa20587120f66
Vincent Xie <vxie0119@MacBook-Pro.lan>
2024-04-10 23:55:30 -0400

    copying starter code from aws

--- hw09/thumbnail_generator.py
+++ hw09/thumbnail_generator.py
@@ -1,1 +1,8 @@
-# initializing
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
