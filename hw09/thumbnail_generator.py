73d66c161cd91a23b98fd4668b6cb0f1fd92b073
Vincent Xie <vxie0119@MacBook-Pro.local>
2024-04-12 13:22:24 -0400

    changing code

--- hw09/thumbnail_generator.py
+++ hw09/thumbnail_generator.py
@@ -6,28 +6,3 @@ import io
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
-    for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key']
-        image = get_image(bucket, key)
-        thumbnail = create_thumbnail(image)
-        s3_save(key, thumbnail)
-
-def get_image(bucket, key):
-    response = s3_client.get_object(Bucket=bucket, Key=key)
-    content = response['Body'].read()
-    image = Image.open(io.BytesIO(content))
-    return image
-
-def create_thumbnail(image):
-    thumbnail_size = (128, 128)
-    image.thumbnail(thumbnail_size)
-    buffer = io.BytesIO()
-    image.save(buffer, 'JPEG')
-    buffer.seek(0)
-    return buffer
-
-def s3_save(key, thumbnail):
-    thumbnail_bucket = 'hw09-thumbnails-vx'
-    thumbnail_key = 'thumbnails/' + os.path.basename(key)
-    s3_client.put_object(Bucket=thumbnail_bucket, Key=thumbnail_key, Body=thumbnail)
